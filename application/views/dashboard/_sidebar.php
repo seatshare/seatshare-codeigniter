d1eeb074055fe5ff82738f7f3e76e783201a17b7
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-10-04 22:29:12 -0500

    Sidebar cleanup

--- application/views/dashboard/_sidebar.php
+++ application/views/dashboard/_sidebar.php
@@ -1,6 +1,8 @@
-<ul class="list-inline">
-	<li><a href="<?php echo site_url('tickets/create_season'); ?>" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-calendar"></span> Add Season Tickets</a></li>			
-</ul>
+<div class="well text-center">
+	<ul class="list-inline" style="margin-bottom:0;">
+		<li><a href="<?php echo site_url('tickets/create_season'); ?>" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-calendar"></span> Add Season Tickets</a></li>			
+	</ul>
+</div>
 
 <?php echo form_fieldset('Group members'); ?>
 
@@ -13,24 +15,17 @@
 <?php echo form_fieldset_close(); ?>
 
 
-<?php echo form_open('groups/invite', array('role'=>'form', 'class' => 'form-horizontal')); ?>
+<?php echo form_open('groups/invite', array('role'=>'form', 'class' => 'form-inline')); ?>
 
 <div class="row">
 	<div class="col-md-12">
 		<?php echo form_fieldset('Invite members'); ?>
 		<div class="form-group">
-			<?php echo form_label('Email Address', 'email', array('class'=>'col-lg-3 control-label')); ?>
-			<div class="col-lg-9">
-				<?php echo form_input(array('name'=>'email', 'class'=>'form-control', 'placeholder'=>'someone@example.com', 'value'=>$this->input->post('email'))); ?>
-			</div>
+			<?php echo form_label('Email Address', 'email', array('class'=>'sr-only')); ?>
+			<?php echo form_input(array('name'=>'email', 'class'=>'form-control', 'style'=>'width:240px;', 'placeholder'=>'someone@example.com', 'value'=>$this->input->post('email'))); ?>
 		</div>
-		<?php echo form_fieldset_close(); ?>
-	</div>
-</div>
-
-<div class="row">
-	<div style="text-align:right;">
 		<?php echo form_submit(array('value'=>'Send Invite', 'class'=>'btn btn-primary')); ?>
+		<?php echo form_fieldset_close(); ?>
 	</div>
 </div>
 
