fed8881f28d74ad1442858cdf5ed24afabebf420
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-10-05 16:27:58 -0500

    Allow users to go with their username or email address

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -20,7 +20,7 @@ class User_Model extends CI_Model {
 	 */
 	public function login( $username='', $password='' ) {
 		$this->db->select( $this->public_fields );
-		$this->db->where('username', $username);
+		$this->db->where(sprintf('(username = "%s" OR email = "%s")', $username, $username));
 		$this->db->where('password', md5($password . $this->config->item('encryption_key')));
 		$this->db->where('status', 1);
 		$query = $this->db->get('users');
