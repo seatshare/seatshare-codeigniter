c87c3a0070136027a91ee65e952da4a6cb67ab2a
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-10-07 19:29:27 -0500

    Context for join step

--- application/views/groups/join_group.php
+++ application/views/groups/join_group.php
@@ -1,24 +1,20 @@
+
 <?php echo form_open('', array('role'=>'form', 'class' => 'form-horizontal')); ?>
 
 <?php echo (validation_errors()) ? '<div class="alert alert-danger">' . validation_errors() . '</div>' : ''; ?>
 
-<div class="row">
-	<div class="col-md-12">
-		<?php echo form_fieldset('Join a Group'); ?>
-		<div class="form-group">
-			<?php echo form_label('Invitation Code', 'invitation_code', array('class'=>'col-md-3 control-label')); ?>
-			<div class="col-md-9">
-				<?php echo form_input(array('name'=>'invitation_code', 'class'=>'form-control', 'value'=>$this->input->get_post('invitation_code'))); ?>
-			</div>
-		</div>
-		<?php echo form_fieldset_close(); ?>
+<?php echo form_fieldset('Join a Group'); ?>
+<p class="lead">To join a group, please enter the invitation code provided to you from the group administrator. It may have been included in an email.</p>
+<div class="form-group">
+	<?php echo form_label('Invitation Code', 'invitation_code', array('class'=>'col-md-3 control-label')); ?>
+	<div class="col-md-9">
+		<?php echo form_input(array('name'=>'invitation_code', 'class'=>'form-control', 'placeholder'=>'ABCDEFG123', 'value'=>$this->input->get_post('invitation_code'))); ?>
 	</div>
 </div>
+<?php echo form_fieldset_close(); ?>
 
-<div class="row">
-	<div style="text-align:right;">
-		<?php echo form_submit(array('value'=>'Join the Group', 'class'=>'btn btn-primary')); ?>
-	</div>
+<div style="text-align:right;">
+	<?php echo form_submit(array('value'=>'Join the Group', 'class'=>'btn btn-primary')); ?>
 </div>
 
 <?php echo form_close(); ?>
