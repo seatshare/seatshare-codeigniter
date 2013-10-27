f855944c1ede0a5fd8ee9489c29e6b4d8a26219d
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-10-27 17:24:13 -0500

    Don't allow autocorrect on email/username fields

--- application/views/login/login_form.php
+++ application/views/login/login_form.php
@@ -1,6 +1,6 @@
 <?php echo form_open('login', array('class'=>'form-signin')); ?>
 
-	<?php echo form_input(array('name'=>'username', 'class'=>'form-control', 'placeholder'=>'Username or email address', 'autofocus'=>'autofocus')); ?>
+	<?php echo form_input(array('name'=>'username', 'class'=>'form-control', 'placeholder'=>'Username or email address', 'autofocus'=>'autofocus', 'autocapitalize'=>'off', 'autocorrect'=>'off')); ?>
 	<?php echo form_password(array('name'=>'password', 'class'=>'form-control', 'placeholder'=>'Password')); ?>
 	<button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
 
