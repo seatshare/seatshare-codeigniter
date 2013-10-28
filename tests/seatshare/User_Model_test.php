075842ff64fc844cd3723a99ef56e742b5f409a0
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-10-28 17:23:11 -0500

    Draft of test suite.

---
+++ tests/seatshare/User_Model_test.php
@@ -0,0 +1,26 @@
+<?php
+
+class User_Model_test extends CI_TestCase {
+
+	public function set_up()
+	{
+		
+	}
+
+	public function test_create_user() {
+		$this->markTestIncomplete('Not yet implemented.');
+	}
+
+	public function test_login() {
+		$this->markTestIncomplete('Not yet implemented.');
+	}
+
+	public function test_login_failed() {
+		$this->markTestIncomplete('Not yet implemented.');
+	}
+
+	public function test_request_password() {
+		$this->markTestIncomplete('Not yet implemented.');
+	}
+
+}
