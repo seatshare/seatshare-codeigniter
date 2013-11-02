6fa920ea80fbbf8bdef107b5b80c8695cb3e4c04
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-11-01 23:13:12 -0500

    Fix a few leftover issues

--- application/views/layouts/home.php
+++ application/views/layouts/home.php
@@ -1,23 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 <title><?php echo $this->config->item('application_name'); ?></title>
+
+<meta charset="utf-8">
 
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('/assets/libraries/bootstrap/css/bootstrap.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('/assets/libraries/clndr/clndr.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('/assets/libraries/jquery.growl/stylesheets/jquery.growl.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('/assets/css/seatshare.css'); ?>">
-
-<link rel="apple-touch-icon" href="<?php echo site_url('assets/images/touch-icon-iphone.png'); ?>">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo site_url('assets/images/touch-icon-ipad.png'); ?>">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo site_url('assets/images/touch-icon-iphone-retina.png'); ?>">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo site_url('assets/images/touch-icon-ipad-retina.png'); ?>">
 <link rel="icon" type="image/png" href="<?php echo site_url('assets/images/favicon.png'); ?>">
 
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="apple-mobile-web-app-title" content="<?php echo $this->config->item('application_name'); ?>">
-<meta name="apple-mobile-web-app-capable" content="yes" />
+<?php include_once('_ios.php'); ?>
 
 <?php include_once('_mixpanel.php'); ?>
 
@@ -136,7 +131,7 @@
 <script src="<?php echo site_url('/assets/libraries/clndr/clndr.js'); ?>"> </script>
 <script src="<?php echo site_url('/assets/libraries/purl/purl.js'); ?>"> </script>
 <script src="<?php echo site_url('/assets/libraries/jquery.growl/javascripts/jquery.growl.js'); ?>"> </script>
-<script src="<?php echo site_url('/assets/js/seatshare.js'); ?>"> </script>
+<script src="<?php echo site_url('/assets/js/seatshare.js?v=1'); ?>"> </script>
 
 <?php include_once '_analytics.php'; ?>
 
--- application/views/layouts/login.php
+++ application/views/layouts/login.php
@@ -1,23 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <title><?php echo ($this->template->getPageTitle()) ? $this->template->getPageTitle() : $this->config->item('application_name'); ?></title>
 
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('/assets/libraries/bootstrap/css/bootstrap.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('/assets/libraries/jquery.growl/stylesheets/jquery.growl.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('/assets/css/login.css'); ?>">
-
-<link rel="apple-touch-icon" href="<?php echo site_url('assets/images/touch-icon-iphone.png'); ?>">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo site_url('assets/images/touch-icon-ipad.png'); ?>">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo site_url('assets/images/touch-icon-iphone-retina.png'); ?>">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo site_url('assets/images/touch-icon-ipad-retina.png'); ?>">
 <link rel="icon" type="image/png" href="<?php echo site_url('assets/images/favicon.png'); ?>">
 
-<meta name="apple-mobile-web-app-title" content="<?php echo $this->config->item('application_name'); ?>">
-<meta name="apple-mobile-web-app-capable" content="yes" />
+<?php include_once('_ios.php'); ?>
 
 <?php include_once('_mixpanel.php'); ?>
 
@@ -25,7 +19,7 @@
 
 </head>
 
-<body>
+<body class="login">
 
 <div class="container">
 
