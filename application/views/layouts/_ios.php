66cb34f75c0361b26eb9946fcacf03f34fbc6f3c
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-11-09 20:08:58 -0600

    Adds a 'Add to Home Scren' bubble

--- application/views/layouts/_ios.php
+++ application/views/layouts/_ios.php
@@ -4,5 +4,6 @@
 <link rel="apple-touch-icon" sizes="152x152" href="<?php echo site_url('assets/images/touch-icon-ipad-retina.png'); ?>">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 <meta name="apple-mobile-web-app-title" content="SeatShare">
 <meta name="apple-mobile-web-app-capable" content="yes" />
